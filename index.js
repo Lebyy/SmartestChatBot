92cf1c904a8544a732b10a4952a616e62f6e24df
udit2303 <69803864+udit2303@users.noreply.github.com>
2021-01-06 14:54:09 +0530

    Update Params

--- index.js
+++ index.js
@@ -2,26 +2,15 @@ const base = 'https://api.affiliateplus.xyz/api'
 const fetch = require("node-fetch");
 const err = require('./Error');
     /**
-     * @param {string} message - The message provided 
-     * @param {string} name - The name of the bot
-     * @param {string} owner - The owner of the bot
-     * @param {string} user - The user id
+     * @param {object} ops - The options
+     * @param {string} ops.message - The Message
+     * @param {string} ops.name - The name of the bot
+     * @param {string} ops.owner - The owner of the bot
+     * @param {string} ops.user - The user id
      * @returns {string} The message by the chatbot
      **/
-    const chat = async (message, name, owner, user) => {
-        if(!message) throw new err("No message was provided");
-        if(!name) {
-        name = "SmartestChatBot"
-        }
-        if(!owner){
-        owner = "Lebyy_Dev"
-        }
-        if(!user){
-        user = "1"
-        }
-        if(typeof(name)!= 'string') throw new err(`Expected name to be string, recieved ${typeof(name)}`);
-        if(typeof(owner)!= 'string') throw new err(`Expected owner to be string, recieved ${typeof(owner)}`);
-        if(typeof(user)!= 'string') throw new err(`Expected user id to be string, recieved ${typeof(user)}`);
+    const chat = async (ops = {message, name:"SmartestChatbot", owner:"Lebyy_Dev", user:'1'}) => {
+        if(!ops.message) throw new err("No message was provided");
         const res = await fetch(`${base}/chatbot?message=${encodeURIComponent(message)}&botname=${encodeURIComponent(name)}&ownername=${encodeURIComponent(owner)}&user=${encodeURIComponent(user)}`,{
         });
         const response = await res.json();
